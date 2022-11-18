3cf9c88bcc21c2fd86b90e4ae35e07d256e3e026
Remigiusz Pisarski <remigiusz.pisarski@axxiomehealth.com>
2022-11-18 11:30:12 +0100

    add api error handling

--- banking-app/src/main/java/com/bankingapp/bankingapp/rest/AccountOperationController.java
+++ banking-app/src/main/java/com/bankingapp/bankingapp/rest/AccountOperationController.java
@@ -3,6 +3,7 @@ package com.bankingapp.bankingapp.rest;
 import com.bankingapp.bankingapp.DTO.CashOperationRequest;
 import com.bankingapp.bankingapp.service.UserAccountService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.print.attribute.standard.Media;
 import javax.validation.Valid;
 
 @AllArgsConstructor
@@ -20,7 +22,7 @@ public class AccountOperationController {
 
     private final UserAccountService userAccountService;
 
-    @PostMapping("/payment")
+    @PostMapping(value = "/payment", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.TEXT_PLAIN_VALUE)
     public ResponseEntity<String> payment(@RequestBody @Valid CashOperationRequest cashOperationRequest) {
         return ResponseEntity.ok(userAccountService.addCashToUser(
                 cashOperationRequest.getUserId(),
@@ -28,7 +30,7 @@ public class AccountOperationController {
         );
     }
 
-    @PostMapping("/paycheck")
+    @PostMapping(value = "/paycheck", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.TEXT_PLAIN_VALUE)
     public ResponseEntity<String> paycheck(@RequestBody @Valid CashOperationRequest cashOperationRequest) {
         return ResponseEntity.ok(userAccountService.takeCashFromAccount(
                 cashOperationRequest.getUserId(),
