36314fc29c54b95a34a9270a5ad60301b0afd975
Remigiusz Pisarski <remigiusz.pisarski@axxiomehealth.com>
2022-11-24 12:03:18 +0100

    refactor makeAPayment method

--- cashmachine/src/main/java/cash/machine/cashmachine/endpoint/OperationController.java
+++ cashmachine/src/main/java/cash/machine/cashmachine/endpoint/OperationController.java
@@ -1,8 +1,7 @@
 package cash.machine.cashmachine.endpoint;
 
-import cash.machine.cashmachine.models.AuthenticationEntity;
-import cash.machine.cashmachine.models.MoneyEntity;
 import cash.machine.cashmachine.models.OperationEntity;
+import com.client.openfeign.clients.BankingAppClient;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -10,76 +9,35 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.net.URISyntaxException;
 import java.net.http.HttpClient;
-import java.util.concurrent.ExecutionException;
+import java.util.Objects;
 
 @AllArgsConstructor
 @Controller
 @RequestMapping("/cash-machine-api")
 public class OperationController {
 
-    private final SystemAppProxy systemAppProxy;
+    private final BankingAppClient bankingAppClient;
 
     private final HttpClient httpClient = HttpClient.newHttpClient();
 
     @PostMapping("/payment")
     public ResponseEntity<String> makeAPayment(
             @RequestBody OperationEntity operationEntity
-            ) throws URISyntaxException, ExecutionException, InterruptedException {
+            ) {
 
-        var verifying = systemAppProxy.verifyCardAndTransaction(
-                new AuthenticationEntity(operationEntity.getCardID(), operationEntity.getCardPIN())
+        var verifying = bankingAppClient.isPINCorrect(
+                operationEntity.getCardID(), operationEntity.getCardPIN()
         ).getBody();
 
         if (verifying == null || !verifying)
             return ResponseEntity.status(403).build();
 
-        var response = systemAppProxy.addCashToAccount(
-                new MoneyEntity(operationEntity.getCardID(), operationEntity.getAmountOfMoney())
-        ).getBody();
+        var response = Objects.requireNonNull(bankingAppClient.addCashToAccount(
+                operationEntity.getCardID(), operationEntity.getAmountOfMoney()
+        ).getBody()).toString();
 
         return ResponseEntity.ok(response);
-/*
-        // TODO to remove quickly
-        var getToken = HttpRequest.newBuilder()
-                .uri(new URI("http://localhost:8081/api/auth/login"))
-                .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(
-                        new JSONObject()
-                                .put("login", "remmo1")
-                                .put("password", "1234abcd")
-                                .toString()
-                ))
-                .build();
-
-        CompletableFuture<HttpResponse<String>> token = HttpClient.newBuilder()
-                .build()
-                .sendAsync(getToken, HttpResponse.BodyHandlers.ofString());
-        System.out.println(token.get().body());
-
-        var request = HttpRequest.newBuilder()
-                .uri(new URI("http://localhost:8081/api/operations/payment"))
-                .header("Content-Type", "application/json")
-                .header("Authorization", "Bearer " + token.get().body())
-                .POST(HttpRequest.BodyPublishers.ofString(
-                        new JSONObject()
-                                .put("userId", 2)
-                                .put("cash", 100)
-                                .toString()
-                ))
-                .build();
-
-        CompletableFuture<HttpResponse<String>>response = HttpClient.newBuilder()
-                .build()
-                .sendAsync(request, HttpResponse.BodyHandlers.ofString());
-
-        try {
-            return ResponseEntity.ok(response.get().body());
-        } catch (InterruptedException | ExecutionException e) {
-            throw new RuntimeException(e);
-        }*/
-
     }
 
 }
