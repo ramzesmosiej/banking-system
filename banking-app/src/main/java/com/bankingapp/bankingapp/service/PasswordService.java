db39b47dbe5e153f3a968f355b44ed6fb837a496
Remigiusz Pisarski <remigiusz.pisarski@axxiomehealth.com>
2022-11-25 12:26:04 +0100

    add test to password encoder service

--- banking-app/src/main/java/com/bankingapp/bankingapp/service/PasswordService.java
+++ banking-app/src/main/java/com/bankingapp/bankingapp/service/PasswordService.java
@@ -1,18 +1,17 @@
 package com.bankingapp.bankingapp.service;
 
+import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+@AllArgsConstructor
 @Service
 public class PasswordService {
 
     private final BCryptPasswordEncoder encoder;
 
-    public PasswordService(BCryptPasswordEncoder encoder) {
-        this.encoder = encoder;
-    }
-
     public String hashPassword(String raw) {
         return encoder.encode(raw);
     }
+
 }
