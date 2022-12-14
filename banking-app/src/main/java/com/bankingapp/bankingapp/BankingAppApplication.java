21a80a0a67c77ff12e97ee6ce3f08dff2c0e1b91
Remigiusz Pisarski <remigiusz.pisarski@axxiomehealth.com>
2022-12-14 09:49:01 +0100

    connect to kafka

--- banking-app/src/main/java/com/bankingapp/bankingapp/BankingAppApplication.java
+++ banking-app/src/main/java/com/bankingapp/bankingapp/BankingAppApplication.java
@@ -10,12 +10,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Set;
 
 
 @AllArgsConstructor
+@EnableKafka
 @SpringBootApplication
 public class BankingAppApplication {
 
