22385b295c87f8dacf06f83e58caafa944d10102
Remigiusz Pisarski <remigiusz.pisarski@axxiomehealth.com>
2022-12-16 11:44:27 +0100

    change properties

--- cashmachine/src/main/java/cash/machine/cashmachine/services/PropertiesConnector.java
+++ cashmachine/src/main/java/cash/machine/cashmachine/services/PropertiesConnector.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@ConfigurationProperties(prefix = "cash.machine.dominikanski")
+@ConfigurationProperties(prefix = "cash.machine")
 @Configuration
 @Getter
 @Setter
